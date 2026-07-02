705cd833a873a69f2f9322f5e8cf94b9e020eab3
SawaTasaki <xj558y2d0290acc@gmail.com>
2026-07-03 08:50:15 +0900

    データ改定

--- react/src/data/novelties.ts
+++ react/src/data/novelties.ts
@@ -9,177 +9,169 @@ export type Novelty = {
 };
 
 export const novelties: Novelty[] = [
-  // OpenAI
   {
     id: "1",
-    companyName: "OpenAI",
-    eventName: "AI EXPO 2026",
+    companyName: "企業１",
+    eventName: "イベント１",
     thumbnailUrl: reactImg,
     pdfUrl:
       "https://drive.google.com/file/d/1pdK-vLXE2DGpaDvcVTTIwjPdl64dKGAR/preview",
   },
   {
     id: "2",
-    companyName: "OpenAI",
-    eventName: "Startup Summit 2026",
+    companyName: "企業１",
+    eventName: "イベント２",
     thumbnailUrl: reactImg,
     pdfUrl:
       "https://drive.google.com/file/d/1pdK-vLXE2DGpaDvcVTTIwjPdl64dKGAR/preview",
   },
 
-  // AWS
   {
     id: "3",
-    companyName: "AWS",
-    eventName: "Interop Tokyo",
+    companyName: "企業２",
+    eventName: "イベント３",
     thumbnailUrl: reactImg,
     pdfUrl:
       "https://drive.google.com/file/d/1pdK-vLXE2DGpaDvcVTTIwjPdl64dKGAR/preview",
   },
   {
     id: "4",
-    companyName: "AWS",
-    eventName: "Cloud Conference 2026",
+    companyName: "企業２",
+    eventName: "イベント４",
     thumbnailUrl: reactImg,
     pdfUrl:
       "https://drive.google.com/file/d/1pdK-vLXE2DGpaDvcVTTIwjPdl64dKGAR/preview",
   },
 
-  // AI EXPO
   {
     id: "5",
-    companyName: "Google",
-    eventName: "AI EXPO 2026",
+    companyName: "企業３",
+    eventName: "イベント１",
     thumbnailUrl: reactImg,
     pdfUrl:
       "https://drive.google.com/file/d/1pdK-vLXE2DGpaDvcVTTIwjPdl64dKGAR/preview",
   },
   {
     id: "6",
-    companyName: "Anthropic",
-    eventName: "AI EXPO 2026",
+    companyName: "企業４",
+    eventName: "イベント１",
     thumbnailUrl: reactImg,
     pdfUrl:
       "https://drive.google.com/file/d/1pdK-vLXE2DGpaDvcVTTIwjPdl64dKGAR/preview",
   },
   {
     id: "7",
-    companyName: "Meta",
-    eventName: "AI EXPO 2026",
+    companyName: "企業５",
+    eventName: "イベント１",
     thumbnailUrl: reactImg,
     pdfUrl:
       "https://drive.google.com/file/d/1pdK-vLXE2DGpaDvcVTTIwjPdl64dKGAR/preview",
   },
   {
     id: "8",
-    companyName: "NVIDIA",
-    eventName: "AI EXPO 2026",
+    companyName: "企業６",
+    eventName: "イベント１",
     thumbnailUrl: reactImg,
     pdfUrl:
       "https://drive.google.com/file/d/1pdK-vLXE2DGpaDvcVTTIwjPdl64dKGAR/preview",
   },
 
-  // Dev Summit
   {
     id: "9",
-    companyName: "Stripe",
-    eventName: "Dev Summit 2026",
+    companyName: "企業７",
+    eventName: "イベント２",
     thumbnailUrl: reactImg,
     pdfUrl:
       "https://drive.google.com/file/d/1pdK-vLXE2DGpaDvcVTTIwjPdl64dKGAR/preview",
   },
   {
     id: "10",
-    companyName: "GitHub",
-    eventName: "Dev Summit 2026",
+    companyName: "企業８",
+    eventName: "イベント２",
     thumbnailUrl: reactImg,
     pdfUrl:
       "https://drive.google.com/file/d/1pdK-vLXE2DGpaDvcVTTIwjPdl64dKGAR/preview",
   },
 
-  // Interop Tokyo
   {
     id: "11",
-    companyName: "Cisco",
-    eventName: "Interop Tokyo",
+    companyName: "企業９",
+    eventName: "イベント５",
     thumbnailUrl: reactImg,
     pdfUrl:
       "https://drive.google.com/file/d/1pdK-vLXE2DGpaDvcVTTIwjPdl64dKGAR/preview",
   },
   {
     id: "12",
-    companyName: "Datadog",
-    eventName: "Interop Tokyo",
+    companyName: "企業１０",
+    eventName: "イベント５",
     thumbnailUrl: reactImg,
     pdfUrl:
       "https://drive.google.com/file/d/1pdK-vLXE2DGpaDvcVTTIwjPdl64dKGAR/preview",
   },
   {
     id: "13",
-    companyName: "Cloudflare",
-    eventName: "Interop Tokyo",
+    companyName: "企業１１",
+    eventName: "イベント５",
     thumbnailUrl: reactImg,
     pdfUrl:
       "https://drive.google.com/file/d/1pdK-vLXE2DGpaDvcVTTIwjPdl64dKGAR/preview",
   },
 
-  // Startup Summit
   {
     id: "14",
-    companyName: "Notion",
-    eventName: "Startup Summit 2026",
+    companyName: "企業１２",
+    eventName: "イベント６",
     thumbnailUrl: reactImg,
     pdfUrl:
       "https://drive.google.com/file/d/1pdK-vLXE2DGpaDvcVTTIwjPdl64dKGAR/preview",
   },
   {
     id: "15",
-    companyName: "Figma",
-    eventName: "Startup Summit 2026",
+    companyName: "企業１３",
+    eventName: "イベント６",
     thumbnailUrl: reactImg,
     pdfUrl:
       "https://drive.google.com/file/d/1pdK-vLXE2DGpaDvcVTTIwjPdl64dKGAR/preview",
   },
   {
     id: "16",
-    companyName: "Vercel",
-    eventName: "Startup Summit 2026",
+    companyName: "企業１４",
+    eventName: "イベント６",
     thumbnailUrl: reactImg,
     pdfUrl:
       "https://drive.google.com/file/d/1pdK-vLXE2DGpaDvcVTTIwjPdl64dKGAR/preview",
   },
 
-  // Cloud Conference
   {
     id: "17",
-    companyName: "Snowflake",
-    eventName: "Cloud Conference 2026",
+    companyName: "企業１５",
+    eventName: "イベント７",
     thumbnailUrl: reactImg,
     pdfUrl:
       "https://drive.google.com/file/d/1pdK-vLXE2DGpaDvcVTTIwjPdl64dKGAR/preview",
   },
   {
     id: "18",
-    companyName: "MongoDB",
-    eventName: "Cloud Conference 2026",
+    companyName: "企業１６",
+    eventName: "イベント７",
     thumbnailUrl: reactImg,
     pdfUrl:
       "https://drive.google.com/file/d/1pdK-vLXE2DGpaDvcVTTIwjPdl64dKGAR/preview",
   },
 
-  // 同じ企業を別イベントにも出す
   {
     id: "19",
-    companyName: "Google",
-    eventName: "Dev Summit 2026",
+    companyName: "企業１７",
+    eventName: "イベント１",
     thumbnailUrl: reactImg,
     pdfUrl:
       "https://drive.google.com/file/d/1pdK-vLXE2DGpaDvcVTTIwjPdl64dKGAR/preview",
   },
   {
     id: "20",
-    companyName: "GitHub",
-    eventName: "AI EXPO 2026",
+    companyName: "企業１８",
+    eventName: "イベント２",
     thumbnailUrl: reactImg,
     pdfUrl:
       "https://drive.google.com/file/d/1pdK-vLXE2DGpaDvcVTTIwjPdl64dKGAR/preview",
